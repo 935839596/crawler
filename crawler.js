b6b873aa9833c1739752eb0b215da5148b0391b8
allen <allenchen@futunn.com>
2017-12-12 20:49:09 +0800

    get data successfully

--- crawler.js
+++ crawler.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var fs = require('fs');
 var cheerio = require('cheerio');
+var jsonfile = require('jsonfile')
 
 // request.get('https://juejin.im/')
 // 	//.auth('15889666430', 'chenruilin1', false)
@@ -14,44 +15,55 @@ var cheerio = require('cheerio');
 var yuming = 'https://juejin.im'
 
 
+
 request('https://juejin.im/welcome/frontend', function(error, response, body){
 	var $ = cheerio.load(body);
 	var lis = $('.title-row .title');
+	var urlArray = [];
 	for(var i=0; i<lis.length; i++){
 		var article = lis.eq(i);
 		var title = article.text(),
-				url =yuming + article.attr('href');
+				url = yuming + article.attr('href');
+		urlArray.push(url)
 		//console.log(url)
 
-    let getArticle = new Promise((resolve, reject) => {
+
+
+	}
+
+  function getArticle(url){
+    return new Promise((resolve, reject) => {
       request(url, function (error, response, body) {
-        var $$ = cheerio.load(body);
-        var author = $$('.author-info .author-name').text(),
-            time = $$('.author-info .author-meta').text().trim();
-        console.log(url)
-        resolve({
+        var $ = cheerio.load(body);
+        var author = $('.author-info .author-name').eq(0).text(),
+            time = $('.author-info .author-meta').eq(0).text().trim(),
+            title = $('.post-title').eq(0).text();
+        var article = {
           title: title,
           url: url,
           author: author,
           time: time
-        })
+        }
+        resolve(JSON.stringify(article))
       })
     })
+  }
 
-    getArticle.then( article => {
-      console.log( article.title? 'gg':article.title)
+
+  var articleList = Promise.all( urlArray.map( (url) => {
+	  return getArticle(url).then( article => JSON.parse(article) )
+  }))
+
+  articleList.then( articleList => {
+    jsonfile.writeFile('a.json', articleList, function(err) {
+      console.error(err)
     })
-		/*request(url, function (error, response, body) {
-			var $$ = cheerio.load(body);
-			var author = $$('.author-info .author-name').text(),
-					time = $$('.author-info .author-meta').text().trim();
-			if(!author){
-				console.log('wrong', url)
-				return
-			}
-			console.log(author, time)
-    })*/
-	}
+    //console.log(articleList)
+  })
+
+
+
+
 
 	// fs.writeFile('a.json',lis[0],{encoding: 'json'}, function(error){
 	// 	console.log('right')
